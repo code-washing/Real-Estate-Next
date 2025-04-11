e307acb986c8f77bd05811e493b0583b47512eae
deep coder <43486287+code-washing@users.noreply.github.com>
2025-04-11 18:26:21 +0900

    Update use-config.tsx

--- src/client/use-config.tsx
+++ src/client/use-config.tsx
@@ -8,12 +8,12 @@ export type ConfigContext = {
 const Config = createContext<ConfigContext>({});
 
 export type ConfigProviderProps = React.PropsWithChildren<ConfigContext>; 
-export type UseConfig = () => ConfigContext; 
-export const useConfig: UseConfig = () => useContext<ConfigContext>(Config); 
+export type UseConfig = () => ConfigContext;  
+export const useConfig: UseConfig = () => useContext<ConfigContext>(Config);  
 
-export default ({
-  children, 
-  loginUrl = process.env.NEXT_PUBLIC_AUTH0_LOGIN || '/api/auth/login' 
-}: ConfigProviderProps): ReactElement<ConfigContext> => { 
-  return <Config.Provider value={{ loginUrl }}>{children}</Config.Provider>; 
-};
+export default ({  
+  children,    
+  loginUrl = process.env.NEXT_PUBLIC_AUTH0_LOGIN || '/api/auth/login'   
+}: ConfigProviderProps): ReactElement<ConfigContext> => {   
+  return <Config.Provider value={{ loginUrl }}>{children}</Config.Provider>;   
+};  
